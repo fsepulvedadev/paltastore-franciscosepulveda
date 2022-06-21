0d8c38f754a1082113315d14a466b1adccefd8b6
Francisco <panchi_nrc@hotmail.com>
2022-06-21 12:43:40 -0300

    Mejoras en searchbar, cambio en la logica ahora utiliza params para mandar la query al componente, en lugar de context

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { Form, FormControl, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
+import "./SearchBar.css";
 
 const SearchBar = () => {
   const [userInput, setUserInput] = useState("");
@@ -17,7 +18,7 @@ const SearchBar = () => {
         onSubmit={(e) => {
           e.preventDefault();
         }}
-        className="d-flex mx-2 w-100 d-flex  justify-content-center justify-content-md-end mt-2 mt-md-0"
+        className="d-flex  w-100 d-flex me-md-2  justify-content-center justify-content-md-end mt-2 mt-md-0"
       >
         <FormControl
           type="search"
@@ -25,12 +26,12 @@ const SearchBar = () => {
           className="me-2 search-bar"
           aria-label="Search"
           value={userInput}
-          onChange={(e) => setUserInput(e.target.value.toLowerCase())}
+          onChange={(e) => setUserInput(e.target.value)}
         />
-        <Link to="/">
+        <Link to={`/search/${userInput.toLowerCase()}`}>
           <Button
             onClick={(e) => {
-              setBusqueda(userInput);
+              setBusqueda(userInput.toLowerCase());
             }}
             className="palta-btn border-0 py-2 text-center"
           >
--- src/components/SearchContainer/SearchContainer.jsx
+++ src/components/SearchContainer/SearchContainer.jsx
@@ -1,10 +1,81 @@
-import React from "react";
+import { useEffect, useState, useContext } from "react";
 import ItemList from "../ItemList/ItemList";
+import dataItems from "../../ItemsDB";
+import PaltaLogo from "../../assets/logo.svg";
+import { Link, useParams } from "react-router-dom";
+import { CartContext } from "../../context/CartContext";
 
 const SearchContainer = () => {
+  const [Items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { search } = useParams();
+
+  const { setBusqueda } = useContext(CartContext);
+
+  // EFECTO PARA BUSCAR
+  useEffect(() => {
+    if (search) {
+      setLoading(true);
+      setTimeout(() => {
+        setItems(
+          dataItems.filter((item) =>
+            item.name.toLowerCase().normalize().includes(search)
+          )
+        );
+        if (Items.length === 0) {
+          setBusqueda("");
+        }
+        setLoading(false);
+      }, 500);
+    } else {
+      setItems(dataItems);
+    }
+  }, [search]);
+
+  const selectItem = (name, price, desc, cat, stock, img, id) => {
+    setSelectedItem({
+      id: id,
+      name: name,
+      price: price,
+      description: desc,
+      categoria: cat,
+      stock: stock,
+      img: img,
+    });
+  };
+
   return (
-    <div>
-      <ItemList />
+    <div className="container d-flex justify-content-center aling-items-center flex-wrap ">
+      {loading && (
+        <div className="w-100 d-flex justify-content-center aling-items-center palta-spiner-container">
+          <img src={PaltaLogo} alt="Palta Logo" className="palta-spiner" />
+        </div>
+      )}
+      {!loading && !selectedItem && (
+        <div className="container d-flex justify-content-center aling-items-center flex-wrap">
+          {Items.length > 0 && search && (
+            <h1>{`Tu busqueda de ${search} arrojo ${Items.length} resultado/s`}</h1>
+          )}
+          <ItemList selectItem={selectItem} items={Items} />
+        </div>
+      )}
+
+      {!loading && !selectedItem && Items.length === 0 && (
+        <div className="d-flex flex-column justify-content-center align-items-center w-100">
+          <h1 className="text-center">{`Tu busqueda de ${search} no arrojo resultado/s.`}</h1>
+          <button
+            onClick={() => {
+              setBusqueda("");
+            }}
+            className="btn btn-primary palta-btn w-50 align"
+          >
+            <Link className="text-decoration-none text-white" to="/">
+              Volver
+            </Link>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
